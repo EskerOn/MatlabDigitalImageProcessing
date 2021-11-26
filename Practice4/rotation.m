8ee250df6095ca75f7ca06f28dc50d23900b5414
Alejandro Escalante <alejandroescalantehdz@gmail.com>
2021-11-26 14:41:02 -0600

    Rotation Done

--- Practice4/rotation.m
+++ Practice4/rotation.m
@@ -1,14 +1,50 @@
 function Res = rotation(img1, angle)
-    [m, n] = size(img1);
-    Res = zeros(m,n);
-    for i = 1:m
-        for j = 1:n
-            x = (i - m / 2) * cosd(angle) - (j - n / 2) * sind(angle) + m / 2;
-            y = (i - m / 2) * sind(angle) + (j - n / 2) * cosd(angle) + n / 2;
-            if x > 0 && x <= m && y > 0 && y <= n
-                Res(i, j) = img1(round(x), round(y));
+    [x,y] = size(img1);
+    switch mod (angle, 360)
+        case 0
+            Res = img1;
+        case 90
+            Res = img1';
+        case 180
+            Res = img1(end:-1:1, end:-1:1);
+        case 270
+            inverted = img1(end:-1:1, end:-1:1);
+            Res = inverted';
+        otherwise
+            rotationMatrix = [cosd(angle) -sind(angle); sind(angle) cosd(angle)];
+            %center of the image
+            centerX = ceil(x/2);
+            centerY = ceil(y/2);
+            % array with corners of the image
+            corners = [1 1; 1 y; x 1; x y];
+            auxMat = [centerX centerY; centerX centerY; centerX centerY; centerX centerY];
+            % rotate and translate corners
+            corners=corners-auxMat;
+            corners=floor((rotationMatrix*corners')');
+            corners=corners+auxMat;
+            % find min and max corners
+            minX = min(corners(:,1));
+            maxX = max(corners(:,1));
+            minY = min(corners(:,2));
+            maxY = max(corners(:,2));
+            % calculate new size of the image
+            newX = maxX - minX + 1;
+            newY = maxY - minY + 1;
+            newCenterX = floor(newX/2);
+            newCenterY = floor(newY/2);
+            %calculate the new image
+            Res = zeros(newX, newY);
+            for i = 1:newX
+                for j = 1:newY
+                    point = [i-newCenterX; j-newCenterY];
+                    newPoint = rotationMatrix*point;
+                    newPoint = newPoint + [centerX; centerY];
+                    newPoint = floor(newPoint);
+                    if newPoint(1) > 0 && newPoint(1) <= x && newPoint(2) > 0 && newPoint(2) <= y
+                        Res(i,j) = img1(floor(newPoint(1)), floor(newPoint(2)));
+                    end
+                end
             end
-        end
-    end
-    Res = uint8(Res);
+            Res=uint8(Res);
+     end      
 end
